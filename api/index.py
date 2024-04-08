ef5403c8ebe8775a1422afc1068b9293c0920b5f
Tony Lopes <tony@lopesdesign.com>
2024-04-08 19:25:59 -0400

    - import chat component from other template - receive chat form input at /api/chat Flask endpoint

--- api/index.py
+++ api/index.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
-@app.route("/api/python")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# @app.route("/api/python")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+@app.route("/api/chat", methods=['POST'])
+def main():
+    # breakpoint()
+
+    print('request.json', request.json)
+    return 'cats' 
