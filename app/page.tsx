0e2a6456cf432ef0f3f23831a0ff068368faff2f
Tony Lopes <tony@lopesdesign.com>
2024-04-10 09:22:27 -0400

    add title to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -18,13 +18,17 @@ export default function Home() {
     // setGotMessages(false);
   };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-start p-24">
+      <h1 className="text-4xl font-bold mb-10">Forestbot</h1>
       <Chat
         input={input}
         handleInputChange={handleInputChange}
         handleMessageSubmit={handleMessageSubmit}
         messages={messages}
       />
+      <h2 className="test-xl mb-4">
+        Ask questions of the documents and backed by the documents
+      </h2>
     </main>
   );
 }
