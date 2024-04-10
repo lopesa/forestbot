652cb8e7cda5985fdf6819c54acee9fdf208a2eb
Tony Lopes <tony@lopesdesign.com>
2024-04-10 08:20:55 -0400

    load .env in python endpoint

--- api/index.py
+++ api/index.py
@@ -1,14 +1,15 @@
 from flask import Flask, request
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+test_env_variable = os.environ.get('TEST_ENV_VARIABLE')
+# env = os.environ
+# print('env', env)
+
 
 app = Flask(__name__)
 
-# @app.route("/api/python")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
-
 @app.route("/api/chat", methods=['POST'])
 def main():
-    # breakpoint()
-
-    print('request.json', request.json)
     return 'cats' 
