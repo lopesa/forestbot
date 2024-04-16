0fb4df6e7c225a4d30bcbfa2821d94d0ae0efbb5
Connor Haines <connor_haines@outlook.com>
2024-04-16 17:39:25 -0600

    typo

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:5328/api/:path*' : 'src/api/'
+        destination: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:5328/api/:path*' : '/src/api/'
       }
     ];
   }
