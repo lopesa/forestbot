3600c05fddae3c566521c535fa53ec3078228b36
Tony Lopes <tony@lopesdesign.com>
2024-04-19 16:48:59 -0400

    generate a new requirements.txt file and copy out to /backend and /streamlit_app
    - triggers the formatting pre-commit hook, so there's formatting changes

--- streamlit_app/streamlit_chatbot.py
+++ streamlit_app/streamlit_chatbot.py
@@ -14,20 +14,28 @@ if "past" not in st.session_state:
     st.session_state["past"] = []
     print("Initialized st.session_state['past']")
 
+
 def get_text():
     input_text = st.text_input("You: ", "Ask a question", key="input")
     submit_button = st.button("Ask")
     print("Input Text:", input_text)  # Displays the entered text
     return input_text, submit_button
 
-print("Script started or rerun")  # Indicates each time the script is executed or re-executed
+
+print(
+    "Script started or rerun"
+)  # Indicates each time the script is executed or re-executed
 user_input, submitted = get_text()
 
 # Check if the button was pressed before calling Call_RAG.ask
 if submitted and user_input:
-    print("Button pressed with input:", user_input)  # Shows that the button was pressed with input
+    print(
+        "Button pressed with input:", user_input
+    )  # Shows that the button was pressed with input
     output = Call_RAG.ask(user_input)
-    print("Output from Call_RAG.ask:", output)  # Displays the output from the ask function
+    print(
+        "Output from Call_RAG.ask:", output
+    )  # Displays the output from the ask function
     st.session_state["past"].append(user_input)
     st.session_state["generated"].append(output)
     print("Updated st.session_state with new input and output")
@@ -36,7 +44,9 @@ print("Displaying messages")  # Indicates that messages will be displayed
 
 # Display of generated messages and user questions
 if st.session_state["generated"]:
-    print("st.session_state['generated'] contents:", st.session_state["generated"])  # Displays the contents of generated
+    print(
+        "st.session_state['generated'] contents:", st.session_state["generated"]
+    )  # Displays the contents of generated
     for i in range(len(st.session_state["generated"]) - 1, -1, -1):
         message(st.session_state["generated"][i], key=str(i))
         message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
