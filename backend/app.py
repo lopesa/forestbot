1f04ee6b56dd9be35375819bdf5b675a5d63179e
Tony Lopes <tony@lopesdesign.com>
2024-04-19 13:45:04 -0400

    moves the api into a /backend folder and adds gunicorn to serve on app server

---
+++ backend/app.py
@@ -0,0 +1,51 @@
+# from flask import Flask, request
+from flask import Flask
+
+# from dotenv import load_dotenv
+# import os
+# from langchain_openai import ChatOpenAI
+# from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+# # env = os.environ
+
+
+# chat = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.2)
+
+# prompt = ChatPromptTemplate.from_messages(
+#     [
+#         (
+#             "system",
+#             "You are a helpful assistant. Answer all questions to the best of your ability.",
+#         ),
+#         MessagesPlaceholder(variable_name="messages"),
+#     ]
+# )
+
+# chain = prompt | chat
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello, home!"
+
+
+@app.route("/chat")
+def main():
+    return "Hello, chat!"
+
+
+# @app.route("/api/chat", methods=["POST"])
+# def main():
+#     test = chain.invoke(
+#         {
+#             "messages": request.json["messages"],
+#         }
+#     )
+#     try:
+#         res = test.content
+#     except Exception:
+#         res = "Error"
+#     return res
