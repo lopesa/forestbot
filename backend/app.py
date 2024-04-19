02c1e70ff1645e57ffbf8808de304f256ddbb0c1
Tony Lopes <tony@lopesdesign.com>
2024-04-19 14:00:53 -0400

    fix flask-dev-monorepo task in npm and restore /api/chat from hello world to chat

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,25 @@
-# from flask import Flask, request
+from flask import Flask, request
 from flask import Flask
 
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 # import os
-# from langchain_openai import ChatOpenAI
-# from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 # # env = os.environ
+chat = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.2)
 
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant. Answer all questions to the best of your ability.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
 
-# chat = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.2)
-
-# prompt = ChatPromptTemplate.from_messages(
-#     [
-#         (
-#             "system",
-#             "You are a helpful assistant. Answer all questions to the best of your ability.",
-#         ),
-#         MessagesPlaceholder(variable_name="messages"),
-#     ]
-# )
-
-# chain = prompt | chat
+chain = prompt | chat
 
 
 app = Flask(__name__)
@@ -32,20 +30,15 @@ def hello_world():
     return "Hello, home!"
 
 
-@app.route("/chat")
+@app.route("/api/chat", methods=["POST"])
 def main():
-    return "Hello, chat!"
-
-
-# @app.route("/api/chat", methods=["POST"])
-# def main():
-#     test = chain.invoke(
-#         {
-#             "messages": request.json["messages"],
-#         }
-#     )
-#     try:
-#         res = test.content
-#     except Exception:
-#         res = "Error"
-#     return res
+    test = chain.invoke(
+        {
+            "messages": request.json["messages"],
+        }
+    )
+    try:
+        res = test.content
+    except Exception:
+        res = "Error"
+    return res
