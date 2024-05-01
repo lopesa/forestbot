19895c588b6045d43323d56d39e70d2aa2358063
Tony Lopes <tony@lopesdesign.com>
2024-05-01 12:54:20 -0400

    working
    - is streaming from backend, check out:
    curl -X POST http://localhost:5328/api/chat --no-buffer

--- backend_fastapi/app.py
+++ backend_fastapi/app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
-from services.gpt_passthrough import chain
+from services.gpt_passthrough import chain, send_message
+import asyncio
 
 import services.call_rag as call_rag
 
@@ -18,20 +20,56 @@ app = FastAPI()
 class RequestBody(BaseModel):
     messages: list
 
+async def fake_video_streamer():
+    for i in range(10):
+        yield b"some fake video bytes"
+        await asyncio.sleep(0.5)
 
-@app.get("/")
-def hello_world():
-    return "Hello, home!"
+
+# @app.get("/")
+# async def main():
+#     return StreamingResponse(fake_video_streamer())
+# def hello_world():
+#     return "Hello, home!"
 
 
 @app.post("/api/chat")
-async def main(request: Request, body: RequestBody):
-    parsed_request = await request.json()
-    llm_version = parsed_request['llmVersion']
-
-    try:
-        res = call_rag.ask(body.messages[-1]["content"]) if llm_version == "rag-v1" else chain.invoke({"messages": body.messages}) 
-    except Exception:
-        raise HTTPException(status_code=500, detail="Internal Server Error")
+# async def main(request: Request, body: RequestBody):
+async def main(request: Request):
+    return StreamingResponse(fake_video_streamer(), media_type='text/event-stream')
     
-    return res.content if llm_version == "gpt-passthrough" else res
+    # parsed_request = await request.json()
+    # llm_version = parsed_request['llmVersion']
+
+    # # generator = send_message(body.messages[0]['content'])
+    # # return StreamingResponse(generator, media_type="text/event-stream")
+
+    # if llm_version == "rag-v1":
+    #     try:
+    #         res = call_rag.ask(body.messages[-1]["content"])
+    #     except Exception:
+    #         raise HTTPException(status_code=500, detail="Internal Server Error")
+    #     return res.content
+    # else:
+    #     return StreamingResponse(send_message(body.messages[0]['content']), media_type="text/event-stream")
+        # try:
+        #     print("body.messages", body.messages[0]['content'])
+        #     # generator = send_message(body.messages)
+        #     # generator = send_message(body.messages[0]['content'])
+        #     # res = chain.invoke({"messages": body.messages})
+        #     # return StreamingResponse(generator, media_type="text/event-stream")
+        #     return StreamingResponse(send_message(body.messages[0]['content']), media_type="text/event-stream")
+        # except Exception:
+        #     raise HTTPException(status_code=500, detail="Internal Server Error")
+
+
+# async def main(request: Request, body: RequestBody):
+#     parsed_request = await request.json()
+#     llm_version = parsed_request['llmVersion']
+
+#     try:
+#         res = call_rag.ask(body.messages[-1]["content"]) if llm_version == "rag-v1" else chain.invoke({"messages": body.messages}) 
+#     except Exception:
+#         raise HTTPException(status_code=500, detail="Internal Server Error")
+    
+#     return res.content if llm_version == "gpt-passthrough" else res
--- backend_fastapi/services/gpt_passthrough.py
+++ backend_fastapi/services/gpt_passthrough.py
@@ -1,5 +1,11 @@
+from typing import AsyncIterable
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+# from langchain.callbacks.base import BaseCallbackHandler
+
+from langchain.callbacks import AsyncIteratorCallbackHandler
+import asyncio
+from langchain.schema import HumanMessage
 
 
 chat = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.2)
@@ -15,3 +21,34 @@ prompt = ChatPromptTemplate.from_messages(
 )
 
 chain = prompt | chat
+
+async def send_message(content: str) -> AsyncIterable[str]:
+    callback = AsyncIteratorCallbackHandler()
+    model = ChatOpenAI(
+        streaming=True,
+        # max_tokens=10,
+        verbose=True,
+        callbacks=[callback],
+    )
+
+    task = asyncio.create_task(
+        # chat.agenerate(messages=[[HumanMessage(content=content)]])
+        # chain.invoke({"messages": content}) 
+        # model.agenerate(chain.invoke({"messages": content}))
+        model.agenerate(messages=[[HumanMessage(content=content)]])
+    )
+
+    async for token in callback.aiter():
+      print(f"Token: {token}")
+      yield token    
+
+    # try:
+    #     async for token in callback.aiter():
+    #         print(f"Token: {token}")
+    #         yield token
+    # except Exception as e:
+    #     print(f"Caught exception: {e}")
+    # finally:
+    #     callback.done.set()
+
+    # await task
