0f99891b64956d0bb6ef500dea548baa4db827c6
Tony Lopes <tony@lopesdesign.com>
2024-06-07 13:04:10 -0400

    restore things. CORS issue is that it needed to be set up in the DO app settings
    - still need to clean up code after confirming this push fixes

--- backend_fastapi/app.py
+++ backend_fastapi/app.py
@@ -6,16 +6,16 @@ from entities.api_router import router as api_router
 
 app = FastAPI(title="forestinfo-bot")
 
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
 app.include_router(api_router, prefix="/api")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 # for production
 # for local dev
 #for production
