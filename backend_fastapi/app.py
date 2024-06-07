92a82eba5340e452b626d3fc471ce346c8f31903
Tony Lopes <tony@lopesdesign.com>
2024-06-07 11:42:01 -0400

    move middleware addition to fastapi app to the end

--- backend_fastapi/app.py
+++ backend_fastapi/app.py
@@ -5,16 +5,17 @@ from entities.api_router import router as api_router
 
 app = FastAPI(title="forestinfo-bot")
 
-# for production
+app.include_router(api_router, prefix="/api")
+
 app.add_middleware(
     CORSMiddleware,
-    # for local dev
     allow_origins=["*"],
-    #for production
-    # allow_origins=["https://forestbot.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(api_router, prefix="/api")
+# for production
+# for local dev
+#for production
+# allow_origins=["https://forestbot.vercel.app/"],
